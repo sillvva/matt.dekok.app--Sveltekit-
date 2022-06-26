1b77908651d63f4a2667d818442c3fee3f9e45fd
Matt Dekok <matt.dekok@gmail.com>
2022-06-26 14:29:45 -0500

    Add pages

---
+++ src/routes/experience.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { pageProps } from '$lib/store';
+
+	$pageProps = {
+		title: 'Experience',
+    bodyClass: "page-body",
+    menu: true
+	};
+</script>
+
+<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
