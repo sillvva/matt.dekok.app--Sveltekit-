887aab79cfac8b9c50468925b35b7980306326cb
Matt Dekok <matt.dekok@gmail.com>
2022-07-01 18:22:58 -0500

    Debugging cron

--- src/routes/blog/cron.ts
+++ src/routes/blog/cron.ts
@@ -10,6 +10,8 @@ export const post: RequestHandler = async ({ url }) => {
 		const perpage = parseInt(url.searchParams.get('limit') || blogPostsPerPage.toString());
 		const query = url.searchParams.get('s') || "";
 
+		if (url.searchParams.get('test')) await testLog();
+
 		const result = await fetchPosts(getPosts, page, perpage, query);
 		const added = result.added || [];
 		const updated = result.updated || [];
