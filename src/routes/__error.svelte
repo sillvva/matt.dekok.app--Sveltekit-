da0ae8101be1fa45ba6104e5e5946a74a13fdef6
Matt Dekok <matt.dekok@gmail.com>
2022-07-18 10:56:53 -0500

    Error Page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" context="module">
+import type { Load } from "./__types/__error";
+import Article from "$lib/components/page/article.svelte";
+import Section from "$lib/components/page/section.svelte";
+import Code from "$lib/components/blog/renderers/code.svelte";
+import Heading from "$lib/components/blog/renderers/heading.svelte";
+
+export const load: Load = ({ error, status }) => {
+  return {
+    props: {
+      status,
+      error
+    }
+  };
+};
+</script>
+
+<script lang="ts">
+export let status: number;
+export let error: Error;
+</script>
+
+<Article class="w-full md:w-9/12">
+  <Section>
+    <Heading depth={2} class="!mt-4">{status} Error</Heading>
+    {#if error}
+      <p class="mb-4">{error.message}</p>
+      {#if error.stack}
+        <Code text={error.stack} lang="" />
+      {/if}
+    {/if}
+  </Section>
+</Article>
